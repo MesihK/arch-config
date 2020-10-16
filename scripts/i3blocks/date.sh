699280204d4a9d597fbd66452fe1c3c1e8e739d7
Mesih Kilinc <mesihkilinc@gmail.com>
2020-10-16 16:32:24 +0300

    add tracker summerization, fix icons

--- scripts/i3blocks/date.sh
+++ scripts/i3blocks/date.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-echo $LABEL$(TZ='US/Central' date +'%d/%m/%Y %H:%M' && TZ='Turkey' date +' %H:%M')
+case $BLOCK_BUTTON in
+	#3) notify-send 'Date' "$(TZ='US/Central' date +'%d/%m/%Y %H:%M' && TZ='Turkey' date +' %H:%M')" ;;
+	3) notify-send "$(date +'%d/%m/%Y')" "$(cal)" ;;
+esac
+
+echo $LABEL$(date +'%H:%M')
